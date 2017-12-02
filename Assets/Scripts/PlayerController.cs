fb34d958a6d40aa5f8a2fe0c9c07e0b2a52e3997
Juan Repetti <juan.repetti@tecnocratica.net>
2017-12-02 20:39:45 +0100

    Initial Commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+
+	float step = 7.5f;
+	float walkLimit = 22.5f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKeyDown("left") && (transform.position.x < walkLimit))
+//			transform.Translate(Vector3.left * -step);
+			transform.position += Vector3.right * step;
+		
+		if (Input.GetKeyDown("right") && (transform.position.x > -walkLimit))
+			transform.position += Vector3.left * step;
+		
+//		
+//
+//
+//		if (Input.GetButton("Jump")) {
+//			_thrusterForce = Vector3.up * thrusterForce;
+//			SetJointSettings(0f);
+//		} else {
+//			SetJointSettings(jointSpring);
+//		}
+	}
+}
